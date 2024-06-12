363456d34a8e674ae7f17b4feb58ad4faf5f9793
Selivonchyk <selivonchik_n@rocketdata.io>
2024-06-12 11:07:40 +0300

    DEV-0. Change DEFAULT_RENDERER_CLASSES.

--- currency_rates/currency_rates/settings.py
+++ currency_rates/currency_rates/settings.py
@@ -137,6 +137,9 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [],
     'DEFAULT_PERMISSION_CLASSES': [],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
 }
 
 SPECTACULAR_SETTINGS = {
